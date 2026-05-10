96ea71b21f473bd9d4960ce82929f54c58f50d9d
just-me-Paula <pauramsil@alum.us.es>
2026-05-10 17:08:20 +0200

    Add files via upload

---
+++ videominer/src/main/java/aiss/videominer/SecurityConfig.java
@@ -0,0 +1,19 @@
+
+package aissvideominer.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public FilterRegistrationBean<ApiKeyFilter> apiKeyFilterRegistration(ApiKeyFilter filter) {
+        FilterRegistrationBean<ApiKeyFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(filter);
+        registration.addUrlPatterns("/*");
+        registration.setOrder(1);
+        return registration;
+    }
+}
